5b2b79388e717804aef0e2187cd926d9f6361a6c
emmanuel <e.lebray@groupeonepoint.com>
2017-12-21 16:12:13 +0100

    up rollup config for @firebase

--- rollup-config.js
+++ rollup-config.js
@@ -5,7 +5,6 @@ import uglify from 'rollup-plugin-uglify'
 export default {
     input: 'src/main-aot.js',
     sourcemap: false,
-    sourcemapFile: 'dist/app.js.map',
     output: {
         format: 'iife',
         file: 'dist/app.js'
@@ -14,10 +13,10 @@ export default {
         if (warning.code === 'THIS_IS_UNDEFINED') { return; }
         console.warn(warning.message);
     },
-    globals: {
+    /*globals: {
         firebase: 'firebase'
     },
-    external: ['firebase'],
+    external: ['firebase'],*/
     plugins: [
         nodeResolve({
             jsnext: true,
@@ -25,7 +24,7 @@ export default {
             browser: true
         }),
         commonjs({
-            include: 'node_modules/rxjs/**',
+            include: ['node_modules/rxjs/**', 'node_modules/@firebase/**']
         }),
         uglify()
     ]
